6d60b448b47f35d6091bf452cd3f441d1664de5b
saglae <114074479+saglae@users.noreply.github.com>
2024-03-05 21:53:47 +0100

    Added empty containers for the words/abbreviations/etc which will be added soon.

--- Main/Modus_1.cpp
+++ Main/Modus_1.cpp
@@ -1,3 +1,10 @@
+#include "Modus_1.h"
 
+
+Letter letters_difficulty_1[] = {};
+Letter letters_difficulty_2[] = {};
+Letter letters_difficulty_3[] = {};
+Letter letters_difficulty_4[] = {};
+Letter letters_difficulty_5[] = {};
 
 
