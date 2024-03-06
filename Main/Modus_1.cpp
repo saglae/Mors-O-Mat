593969417058f164e82576ea00d00c4d3fc50b67
saglae <114074479+saglae@users.noreply.github.com>
2024-03-06 08:48:04 +0100

    Research on correct order of teh letters for efficient learning (according to Moltrecht)

--- Main/Modus_1.cpp
+++ Main/Modus_1.cpp
@@ -8,3 +8,31 @@ Letter letters_difficulty_4[] = {};
 Letter letters_difficulty_5[] = {};
 
 
+/*
+Moltrecht-Lehrgang: https://dj4uf.de/morsen
+
+1. Lektion: e,s,n,o, Start, Stop, Irrung
+2. Lektion: t,q,5
+3. Lektion: r,u,9
+4. Lektion: Trennungszeichen "--", vorwiegend Wiederholung
+5. Lektion: c,d,0
+6. Lektion: a,l,8
+7. Lektion: i,x,4
+8. Lektion: ., vorwiegend Wiederholung
+9. Lektion: m,7
+10. Lektion: y,1
+11. Lektion: h,6
+12. Lektion: ?, vorwiegend Wiederholung
+13. Lektion: j,3
+14. Lektion: g,2
+15. Lektion: v,k
+16. Lektion: , ,vorwiegend Wiederholung
+17. Lektion: z,f
+18. Lektion: b,p
+19. Lektion: w, /
+20. Lektion: ä,ö,ü
+
+Danach nurnoch auf Geschwindigkeit...
+*/
+
+
